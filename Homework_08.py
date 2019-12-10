6e69390cd6b4cd7b8b96102f5717746a624c5bea
Jan Buchmann <jan.buchmann@gmx.de>
2019-12-10 17:09:41 +0100

    Add replacement of uncommon words with 'UNK' and backoff taggers, accuracy at 90.28 %

--- Homework_08.py
+++ Homework_08.py
@@ -24,19 +24,31 @@
 
 # 8.2
 import nltk
+import re
+import logging
 
 # Class to implement chat tagger
 # "Training" is done with initialization
-# Inspired by https://www.cs.bgu.ac.il/~elhadad/nlp18/NLTKPOSTagging.html
+# General class setup inspired by
+# https://www.cs.bgu.ac.il/~elhadad/nlp18/NLTKPOSTagging.html
 class Chat_Tagger():
     # Training of tagger happens in initialization
     # The tagger is a simple nltk trigram tagger
     def __init__(self, train_posts):
-        self.trigram_tagger = nltk.TrigramTagger(train_posts)
+        self.t1 = nltk.UnigramTagger(train_posts)
+        self.t2 = nltk.BigramTagger(train_posts, backoff=self.t1)
+        self.t3 = nltk.TrigramTagger(train_posts, backoff=self.t2)
+
+        patterns = [
+            (r'^:-*[()P*oD]$', 'X'),
+            (r'^l+o+l+$', 'X'),
+            (r'^r+o+f+l+$', 'X')
+        ]
+        self.rt = nltk.RegexpTagger(patterns, backoff=self.t3)
 
     # Function to tag a single sentence / post
     def tag(self, post):
-        return self.trigram_tagger.tag(post)
+        return self.t3.tag(post)
 
     # Function to tag a list of posts
     def batch_tag(self, posts):
@@ -68,22 +80,77 @@ class Chat_Tagger():
         tagged_posts = self.batch_tag(self.untag(post) for post in gold_posts)
         return self.get_batch_accuracy(gold_posts, tagged_posts)
 
+# Function to replace a word with 'UNK' if it is not in keep_list and cannot
+# be matched to keep_regexp
+def replace_word(word, keep_list, keep_regexp):
+    return word if (
+            (word in keep_list)
+            or any(re.match(pattern, word) for pattern in keep_regexp)
+    ) else 'UNK'
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=('%(asctime)s : %(levelname)s : '
+                '%(module)s : %(funcName)s : %(message)s'),
+        datefmt='%H:%M:%S'
+    )
+
+    logger.info('Starting program')
+
+    # Get complete set of tagged chat posts
     tagged_posts = nltk.corpus.nps_chat.tagged_posts(tagset='universal')
+
+    # Get tagged sentences from the Brown corpus
+    tagged_sents = nltk.corpus.brown.tagged_sents(tagset='universal')
+
+    # Preprocess the training and test data
+    # Get frequency distributions of words
+    chat_fd = nltk.probability.FreqDist(
+        [token[0] for post in tagged_posts for token in post]
+    )
+
+    # Get the most frequent words
+    most_freq_X = 1000
+    chat_most_freq = [
+        t[0] for t in chat_fd.most_common(most_freq_X)
+    ]
+    # brown_most_freq = brown_fd.most_common(most_freq_X)
+
+    # Define regex patterns to keep
+    keep_regexp = [
+        r'^:-*[()P*oD]$',
+        r'^l+o+l+$',
+        r'^r+o+f+l+$'
+    ]
+
+    # Replace every word that is not in most frequent words with 'UNK',
+    # except it is an emoticon
+    logger.info('Starting word replacement')
+    tagged_posts = [
+        [
+            (replace_word(word, chat_most_freq, keep_regexp), tag)
+            for word, tag in post
+        ]
+        for post in tagged_posts
+    ]
+
+    logger.info('Completed word replacement')
+
+    # Get the total number of tagged posts
     nr_posts = len(tagged_posts)
 
+    # Split the posts into training, development and test set
     train_posts = tagged_posts[:(nr_posts * 8) // 10]
     development_posts = tagged_posts[
         (nr_posts * 8) // 10 : (nr_posts * 9) // 10
     ]
     test_posts = tagged_posts[(nr_posts * 9) // 10:]
 
+    # Initialize tagger
     tagger = Chat_Tagger(train_posts)
 
+    # Evaluate tagger
     print(tagger.evaluate(development_posts))
-
-
-
-
-
