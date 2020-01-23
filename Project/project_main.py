77bb3f24ca4b314aae2ec6a8dbf2d1d2b3c418f0
Jan Buchmann <jan.buchmann@gmx.de>
2020-01-23 16:10:22 +0100

    set up basic pipeline, should be functional

--- Project/project_main.py
+++ Project/project_main.py
@@ -2,11 +2,13 @@
 import logging
 import Features
 import Data
+import Classifier
+import nltk
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format=('%(asctime)s : %(levelname)s : %(module)s : %(funcName)s : %(message)s'),
         datefmt='%H:%M:%S'
     )
@@ -18,17 +20,33 @@ if __name__ == '__main__':
     trainingSet, developmentSet = Data.splitDataSet(datapoints)
     logger.info(f"Split data as following: {len(trainingSet)} for training, {len(developmentSet)} for development.")
 
-    if True:
-        feature_list = [
-            Features.getMostCommonWords,
-            Features.getMostCommonWordsCleaned,
-            Features.getPortionOfCapitalWords,
-            Features.getPortionOfPunctuations
-        ]
-        for point in trainingSet:
-            print(point)
-            print("\t", Features.getFeatures(point.comment_text, feature_list))
-            # print("\t", Features.getMostCommonWords(point.comment_text))
-            # print("\t", Features.getMostCommonWordsCleaned(point.comment_text))
-            # print("\t", Features.getPortionOfCapitalWords(point.comment_text))
-            # print("\t", Features.getPortionOfPunctuations(point.comment_text))
+    feature_list = [
+        Features.getUnigramFeatures
+    ]
+
+    NBC = nltk.classify.NaiveBayesClassifier
+
+    classifier = Classifier.Classifier(
+        NBC,
+        Features.getFeatures,
+        feature_list
+    )
+
+    classifier.train(trainingSet)
+
+    print(Classifier.calculateAccuracy(classifier.predict(developmentSet)))
+
+    # if True:
+    #     feature_list = [
+    #         Features.getMostCommonWords,
+    #         Features.getMostCommonWordsCleaned,
+    #         Features.getPortionOfCapitalWords,
+    #         Features.getPortionOfPunctuations
+    #     ]
+    #     for point in trainingSet:
+    #         print(point)
+    #         print("\t", Features.getFeatures(point.comment_text, feature_list))
+    #         # print("\t", Features.getMostCommonWords(point.comment_text))
+    #         # print("\t", Features.getMostCommonWordsCleaned(point.comment_text))
+    #         # print("\t", Features.getPortionOfCapitalWords(point.comment_text))
+    #         # print("\t", Features.getPortionOfPunctuations(point.comment_text))
