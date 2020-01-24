4ba6a1411b0de6385723ccefaee1b6a28342cf9f
Jan Buchmann <jan.buchmann@gmx.de>
2020-01-24 11:21:29 +0100

    add comments

--- Project/Classifier.py
+++ Project/Classifier.py
@@ -55,24 +55,42 @@ def calculateAccuracy(dataset):
             wrongPredictions +=1
     return (len(dataset) - wrongPredictions) / len(dataset)
 
+# Function to test all combinations of a list of feature extraction functions
+# Inputs:
+# train_data: The data for training a classifier as a list of data points in
+# the Data.Datapoint format
+# test_data: The data to test the classifier in the same format as train_data
+# classifier_base: An uninitialized nltk classifier
+# features: A list of feature functions to be tested
+#
+# Returns:
+# 1. A tuple of the best accuracy and the best feature combination
+# 2. A list of tuples of accuracy and corresponding feature combination
 def testFeatureCombinations(train_data, test_data, classifier_base, features, **feature_args):
+    # Get all combinations of the given features
     combinations = list(Features.getAllCombinations(set(features), set()))
 
+    # Empty list to store the accuracies
     accuracy_results = []
 
+    # variable to store the best accuracy value
     best_accuracy = 0
 
+    # Variable to store the best feature combination
     best_feature_combination = tuple()
 
+    # Iterate over all combinations of features
     for feature_tuple in combinations:
+        # Initialize the classifier
         classifier = Classifier(
             classifier_base,
             Features.getFeatures,
             feature_tuple
         )
-
+        # Train
         classifier.train(train_data)
 
+        # Predict and get the accuracy
         accuracy = calculateAccuracy(classifier.predict(test_data))
 
         accuracy_results.append(accuracy)
